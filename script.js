71ab19d8d83ad73c0bc3e8227c593daabba5e574
marhichapata-prog <marhichapata@gmail.com>
2026-06-06 07:52:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// MENU MOBILE
+document.getElementById("menuBtn").addEventListener("click", function () {
+  document.getElementById("menu").classList.toggle("active");
+});
+
+// VALIDASI FORM
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let nama = document.getElementById("nama").value.trim();
+
+  let email = document.getElementById("email").value.trim();
+
+  let subjek = document.getElementById("subjek").value.trim();
+
+  let pesan = document.getElementById("pesan").value.trim();
+
+  if (nama === "" || email === "" || subjek === "" || pesan === "") {
+    alert("Semua field harus diisi!");
+    return;
+  }
+
+  if (!email.includes("@")) {
+    alert("Format email tidak valid!");
+    return;
+  }
+
+  alert("Pesan berhasil dikirim!");
+
+  this.reset();
+});
